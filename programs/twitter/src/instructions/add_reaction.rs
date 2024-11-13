470f682a4ce98bde243d35ce00383cda8f40e8af
lawal abdulrazaq temitope <temitopelawal925@gmail.com>
2024-11-13 19:25:06 +0100

    submission

--- programs/twitter/src/instructions/add_reaction.rs
+++ programs/twitter/src/instructions/add_reaction.rs
@@ -1,3 +1,4 @@
+// add_reaction.rs
 use anchor_lang::prelude::*;
 
 use crate::errors::TwitterError;
@@ -8,55 +9,65 @@ pub fn add_reaction(ctx: Context<AddReactionContext>, reaction: ReactionType) ->
     let tweet_reaction = &mut ctx.accounts.tweet_reaction;
 
     // -------------------------------------------------------------------------------------------
-    // TODO: The input parameter of this function is a reaction, which is of type enum ReactionType.
-    // Based on this fact you should check what type of reaction we are getting and update
-    // the number of likes and dislikes within the tweet Account accordingly. Return an error in
-    // case of over/underflow. Finally save the reaction value to the tweet_reaction variable of
-    // the Reaction data account.
+    // Based on the reaction type (Like or Dislike), update the corresponding count in the tweet.
+    match reaction {
+        ReactionType::Like => {
+            tweet.likes = tweet
+                .likes
+                .checked_add(1)
+                .ok_or(TwitterError::MaxLikesReached)?; // Ensures no overflow of likes
+        }
+        ReactionType::Dislike => {
+            tweet.dislikes = tweet
+                .dislikes
+                .checked_add(1)
+                .ok_or(TwitterError::MaxDislikesReached)?; // Ensures no overflow of dislikes
+        }
+    }
 
-    // HINT1: Try using pattern matching via the match keyword.
-    // https://doc.rust-lang.org/rust-by-example/flow_control/match.html
+    tweet_reaction.reaction = reaction;
 
-    // HINT2: tweet.likes = tweet.likes.checked_add(1).ok_or(TwitterError::MaxLikesReached)?;
     // -------------------------------------------------------------------------------------------
+    // Update the tweet_reaction account with necessary information.
+    tweet_reaction.reaction_author = ctx.accounts.reaction_author.key();  // Store the reaction author's public key
+    tweet_reaction.parent_tweet = ctx.accounts.tweet.key();               // Link to the tweet this reaction belongs to
 
-    // TODO: Do not forget to update all values within the tweet_reaction Account.
-
-    // HINT:
-    // - reaction_author
-    // - parent_tweet
-    // - bump (check initialize_tweet for reference)
-    // -------------------------------------------------------------------------------------------
+    // Fix here: Access the bump for tweet_reaction directly from ctx.bumps
+    tweet_reaction.bump = ctx.bumps.tweet_reaction;      // Get bump from context
 
     Ok(())
 }
+
 #[derive(Accounts)]
 pub struct AddReactionContext<'info> {
     #[account(mut)]
-    pub reaction_author: Signer<'info>,
+    pub reaction_author: Signer<'info>, // The person who is reacting
+
+    // Initialize the tweet_reaction account using a unique seed derived from reaction_author and tweet
     #[account(
         init,
-        payer = reaction_author,
-        space = 8 + Reaction::LEN,
+        payer = reaction_author, 
+        space = 8 + Reaction::LEN,  // Allocate space for the reaction data (you'll want to adjust this size)
         seeds = [
             TWEET_REACTION_SEED.as_bytes(),
             reaction_author.key().as_ref(),
             tweet.key().as_ref(),
-            ],
-        bump)]
-    pub tweet_reaction: Account<'info, Reaction>,
-    // -------------------------------------------------------------------------------------------
-    // TODO: Fill the required account macro below - we want to check that a PDA account with correct
-    // seeds and bump was submitted.
+        ],
+        bump
+    )]
+    pub tweet_reaction: Account<'info, Reaction>,  // The reaction being added
 
-    // HINT:
-    // - account must be mutable
-    // - seeds are :    tweet.topic[..tweet.topic_length as usize].as_ref()
-    //                  TWEET_SEED.as_bytes(),
-    //                  tweet.tweet_author.key().as_ref()
-    // - lastly, check the correctness of bump using: bump = tweet.bump
-    // -------------------------------------------------------------------------------------------
-    #[account()]
-    pub tweet: Account<'info, Tweet>,
-    pub system_program: Program<'info, System>,
+    // Ensure tweet account is mutable and provide seeds for tweet address
+    #[account(
+        mut,
+        seeds = [
+            tweet.topic[..tweet.topic_length as usize].as_ref(),
+            TWEET_SEED.as_bytes(),
+            tweet.tweet_author.key().as_ref(),
+        ],
+        bump = tweet.bump
+    )]
+    pub tweet: Account<'info, Tweet>,  // The tweet being reacted to
+
+    pub system_program: Program<'info, System>,  // System program to initialize the account
 }
